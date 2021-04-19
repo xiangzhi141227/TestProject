f74f3a6f6627cb88499545a23316522af114e9b2
xyz <1415476054@qq.com>
2021-04-19 17:28:18 +0800

    test selenium and webdiver

--- AutoTest/test_driver.py
+++ AutoTest/test_driver.py
@@ -4,12 +4,14 @@ import time
 
 class Test:
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome()  # 注意Chrome的C是大写，
+        # wecdiver.exe文件放在环境变量下可能没有用放在Python安装目录下Script目录下
 
     def test_driver(self):
         self.driver.get("https://www.baidu.com")
+        self.driver.implicitly_wait(3)  # 隐士等待
         self.driver.find_element_by_link_text("贴吧").click()
-        time.sleep(5)
+        time.sleep(5)  # 显示等待
 
     def test_quit(self):
         self.driver.quit()
